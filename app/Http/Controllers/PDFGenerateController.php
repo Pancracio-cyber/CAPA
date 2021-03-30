bc6142638f3b1fbdd367ea651f253ea22719dae5
Ernesto-moreno258 <ernesto.em550@gmail.com>
2021-03-30 19:31:38 +0200

    Cambios en BD y datos del PDF

--- app/Http/Controllers/PDFGenerateController.php
+++ app/Http/Controllers/PDFGenerateController.php
@@ -8,6 +8,7 @@ use App\Token;
 use App\Solicitud;
 use App\Firmante_pdf;
 use SimpleSoftwareIO\QrCode\Facades\QrCode as Qr;
+/*:select("solicitudes.apellido_materno","solicitudes.apellido_paterno","solicitudes.calle1","solicitudes.calle2","solicitudes.colonia","solicitudes.created_at","solicitudes.curp","solicitudes.diametroToma","solicitudes.direccion","solicitudes.fechalimite","solicitudes.fecha_pago","solicitudes.fecha_solicitud","solicitudes.fecha_Solicitud_api","solicitudes.","solicitudes.hora_pago","solicitudes.hora_solicitud","solicitudes.hora_solicitud_api","solicitudes.id","solicitudes.id_electronico","solicitudes.id_estado","solicitudes.id_firmante","solicitudes.id_sello","solicitudes.id_tramite","solicitudes.importe","solicitudes.llave","solicitudes.lote","solicitudes.manzana","solicitudes.nombre as nombre_solicitud","municipios.nombre as nombre_municipio","actividades.texto_alternativo_img","actividades.url_imagen","actividades.descripcion","actividades.id","actividades.nombre","tipo_actividades.nombre as tipo_actividad"):*/
 
 class PDFGenerateController extends Controller
 {
@@ -16,7 +17,8 @@ class PDFGenerateController extends Controller
         if($request["ews_token"]!==Token::first()["token"]){
             return response()->json(["wsp_mensaje"=>"TOKEN Invalido o Inexistente"]);
           }     
-          $solicitud= Solicitud::where("no_solicitud",$request["ews_no_solicitud"])->first();
+          $solicitud= Solicitud::where("no_solicitud",$request["ews_no_solicitud"])-> join('municipios', 'solicitudes.id_municipio', '=', 'municipios.id')->first();
+          //return $solicitud;
           if(!$solicitud)
           {
             return response()->json(["wsp_mensaje"=>"No sev encontro la solicitud"],400);
@@ -130,7 +132,7 @@ class PDFGenerateController extends Controller
                     }
                 $nombrefirmante=$firmantepdf["nombre_firmante"]." ".$firmantepdf["primer_apellido_firmante"]." ".$firmantepdf["segundo_apellido_firmante"];
                 $nombreciudadano = $nombresolicitante.$espaciosEnBlanco.$nombrefirmante;
-                Qr::format('png')->size(200)->generate('Hola Mundo', 'hola.png');
+                Qr::format('png')->size(200)->generate('https://www.potys.gob.mx/validatramite/?id='.$solicitud->id_electronico,"QR".$solicitud->id_electronico.".png");
             $fpdf= new PDF();  
             $calle1= $solicitud["calle1"];
             $calle2= $solicitud["calle2"];
@@ -159,7 +161,7 @@ class PDFGenerateController extends Controller
         $fpdf->Cell(115,0, "");
         $fpdf->Cell(0, 0, utf8_decode($solicitud["no_contrato"]));
         $fpdf->ln(12);
-        $fpdf->Cell(0, 0, utf8_decode($solicitud["nombre"]));
+        $fpdf->Cell(0, 0, utf8_decode($solicitud->nombre." ".$solicitud->apellido_paterno." ".$solicitud->apellido_materno));
         $fpdf->ln(12);
         $fpdf->Cell(0, 0, utf8_decode($solicitud["direccion"]));
         $fpdf->ln(12);
@@ -167,7 +169,7 @@ class PDFGenerateController extends Controller
         $fpdf->ln(12);
         $fpdf->Cell(0, 0, utf8_decode($solicitud["colonia"]));
         $fpdf->ln(10);
-        $fpdf->Cell(0, 0, utf8_decode($municipio));
+        $fpdf->Cell(0, 0, utf8_decode($solicitud["nombre_municipio"]." ".($solicitud["localidad"])));
         $fpdf->ln(12);
         $fpdf->Cell(0, 0, utf8_decode($tipo_servicio));
         $fpdf->ln(12);
@@ -176,16 +178,15 @@ class PDFGenerateController extends Controller
         $fpdf->Cell(0, 0, utf8_decode($solicitud["importe"]));
         $fpdf->ln(18);
         $fpdf->Cell(15,0, "");
-        $fpdf->Cell(0, 0, utf8_decode("Cozumel"."       ".date("d")."   ".$mes."   ".date("Y")));
+        $fpdf->Cell(0, 0, utf8_decode($solicitud["localidad"]."       ".date("d")."   ".$mes."     ".date("Y")));
         $fpdf->ln(30);
         $fpdf->SetFont('Courier', 'B', 15);
-        $fpdf->Cell(0,0,  utf8_decode($nombreciudadano));
+        $fpdf->Cell(0,0, utf8_decode($nombreciudadano));
         $fpdf->ln(5);
-        $fpdf->Image('hola.png',20,262,35);
+        $fpdf->Image("QR".$solicitud->id_electronico.".png",20,262,35);
         $fpdf->AddPage();
         $fpdf->SetTextColor(0,0,0);
         $fpdf->Image('imagenes/Normas.jpeg',0,0,210);
-        //return $fpdf->Output();
-        return $fpdf->Output("F","Daniel.pdf");
+        return $fpdf->Output();
     }
 }
---
+++ database/migrations/2021_03_30_142840_create_solicitudes_table.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSolicitudesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *  
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solicitudes', function (Blueprint $table) {
+            $table->id();
+            $table->string('llave')->nullable();
+            $table->string('id_tramite')->nullable();
+            $table->string('no_solicitud')->nullable();
+            $table->date('fecha_solicitud')->nullable();
+            $table->time('hora_solicitud')->nullable();
+            $table->decimal('no_solicitud_api');
+            $table->time('hora_solicitud_api')->nullable();
+            $table->date('fecha_solicitud_api')->nullable();
+            $table->string('id_estado')->nullable();
+            $table->string('id_electronico')->nullable();
+            $table->string('referencia_pago')->nullable();
+            $table->date('fecha_pago')->nullable();
+            $table->time('hora_pago')->nullable();
+            $table->string('curp', 18)->nullable();
+            $table->string('nombre')->nullable();
+            $table->string('apellido_paterno')->nullable();
+            $table->string('apellido_materno')->nullable();
+            $table->string('direccion')->nullable();
+            $table->string('colonia')->nullable();
+            $table->string('importe')->nullable();
+            $table->date('fechalimite')->nullable();
+            $table->string('sector')->nullable();
+            $table->string('manzana')->nullable();
+            $table->string('lote')->nullable();
+            $table->string('tarifa')->nullable();
+            $table->string('tipo_servicio')->nullable();
+            $table->string('no_medidor')->nullable();
+            $table->string('diametroToma')->nullable();
+            $table->string('stripe_orden_id')->nullable();
+            $table->string('stripe_creado')->nullable();
+            $table->string('stripe_mensaje')->nullable();
+            $table->string('stripe_tipo')->nullable();
+            $table->string('stripe_digitos')->nullable();
+            $table->string('stripe_red')->nullable();
+            $table->string('stripe_estado')->nullable();
+            $table->string('xml_url')->nullable();
+            $table->string('no_consulta')->nullable();
+            $table->integer('no_contrato')->nullable();
+            $table->string('calle1')->nullable();
+            $table->string('calle2')->nullable();
+            $table->string('id_firmante')->nullable();
+            $table->string('id_sello')->nullable();
+            $table->unsignedBigInteger("id_municipio")->nullable();
+            $table->foreign("id_municipio")->references('id')->on('municipios');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('solicitudes');
+    }
+}
